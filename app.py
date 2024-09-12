21372e64814ac84cf6149ece356919d8c03d01d5
AlfredoB14 <alfredobarranco14@gmail.com>
2024-09-12 15:56:10 -0600

    Add CORS support to Flask app

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from dotenv import load_dotenv
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
  
 db = SQLAlchemy(app)
