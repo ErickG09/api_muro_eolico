4e3b098601e38e66009418e6db56d6405bbc5fa6
AlfredoB14 <alfredobarranco14@gmail.com>
2024-09-18 18:30:05 -0600

    Refactor WallData and DayTotal models, add scheduler for daily DayTotal creation

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 import os
 from flask_cors import CORS
 from flask_migrate import Migrate
-from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import date
+import pytz
 
 app = Flask(__name__)
 CORS(app)
@@ -17,7 +17,9 @@ migrate = Migrate(app, db)  # Inicializar Flask-Migrate
 
 BASE_URL = '/api/v1'
  
- 
+mexico_tz = pytz.timezone('America/Mexico_City')
+# -----------------------------------------------------------------------
+# MODELOS
 class WallData(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     propeller1 = db.Column(db.Float, nullable=False)
@@ -25,8 +27,8 @@ class WallData(db.Model):
     propeller3 = db.Column(db.Float, nullable=False)
     propeller4 = db.Column(db.Float, nullable=False)
     propeller5 = db.Column(db.Float, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)  # Ya está correcto
-
+    created_at = db.Column(db.Date, default=lambda: datetime.now(mexico_tz).date())
+    hour = db.Column(db.String(2), default=lambda: datetime.now(mexico_tz).strftime('%H'))  # Use Mexico City time
     def __init__(self, propeller1, propeller2, propeller3, propeller4, propeller5):    
         self.propeller1 = propeller1
         self.propeller2 = propeller2
@@ -42,7 +44,9 @@ class WallData(db.Model):
             'propeller3': self.propeller3,
             'propeller4': self.propeller4,
             'propeller5': self.propeller5,
-            'created_at': self.created_at.strftime('%Y-%m-%d %H:%M:%S')  # Incluir created_at
+            'created_at': self.created_at.strftime('%Y-%m-%d'),  # Incluir created_at
+            'hour': self.hour  # Incluir la hora
+            
         }
 
     def __repr__(self):
@@ -54,7 +58,8 @@ class DayTotal(db.Model):
     grupo1 = db.Column(db.Float, default=0)  # Sumatoria de propeller1 y propeller2
     grupo2 = db.Column(db.Float, default=0)  # Sumatoria de propeller3
     grupo3 = db.Column(db.Float, default=0)  # Sumatoria de propeller4 y propeller5
-    created_at = db.Column(db.Date, default=datetime.utcnow().date())  # Fecha del día
+    total = db.Column(db.Float, default=0)  # Sumatoria de todos los propellers
+    created_at = db.Column(db.Date, default=lambda: datetime.now(mexico_tz).date())  # Use Mexico City time
 
     def to_json(self):
         return {
@@ -62,7 +67,20 @@ class DayTotal(db.Model):
             'grupo1': self.grupo1,
             'grupo2': self.grupo2,
             'grupo3': self.grupo3,
+            'total': self.grupo1 + self.grupo2 + self.grupo3,
             'created_at': self.created_at.strftime('%Y-%m-%d')
+        }
+# -----------------------------------------------------------------------
+class MonthTotal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    total = db.Column(db.Float, default=0)  # Sumatoria de todos los propellers
+    month = db.Column(db.String(2), default=lambda: datetime.now(mexico_tz).strftime('%m'))  # Use Mexico City time
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'total': self.total,
+            'month': self.month
         }
 
 # --- MAIN --------------------------------------------------------------------
@@ -73,6 +91,13 @@ def index():
  
 @app.route(BASE_URL + '/new', methods=['POST'])
 def create():
+
+
+    date = datetime.now(mexico_tz)
+    today = date.strftime("%Y-%m-%d")
+
+    month = date.strftime("%m")
+    #today = date(2024, 9, 19)
     if not request.json or 'propeller1' not in request.json:
         abort(400)
     
@@ -93,23 +118,46 @@ def create():
     grupo1_sum = request.json['propeller1'] + request.json['propeller2']
     grupo2_sum = request.json['propeller3']
     grupo3_sum = request.json['propeller4'] + request.json['propeller5']
+    total_sum = grupo1_sum + grupo2_sum + grupo3_sum
     
     # Actualizar el total en DayTotal (suponiendo que solo tienes un registro en DayTotal)
     day_total = DayTotal.query.first()
     
+    #print(str(day_total.created_at) == str(today))
+
     if not day_total:
         # Si no existe un registro de DayTotal, lo creamos
-        day_total = DayTotal(grupo1=grupo1_sum, grupo2=grupo2_sum, grupo3=grupo3_sum)
+        day_total = DayTotal(grupo1=grupo1_sum, grupo2=grupo2_sum, grupo3=grupo3_sum, total=total_sum, created_at=today)
         db.session.add(day_total)
-    else:
+    
+    elif str(day_total.created_at) == str(today):
         # Si ya existe, lo actualizamos sumando los valores a cada grupo
         day_total.grupo1 += grupo1_sum
         day_total.grupo2 += grupo2_sum
         day_total.grupo3 += grupo3_sum
+        day_total.total += total_sum
+
+    else:
+        # Si el registro de DayTotal no es de hoy, lo creamos
+        day_total = DayTotal(grupo1=grupo1_sum, grupo2=grupo2_sum, grupo3=grupo3_sum, total = total_sum, created_at=today)
+        db.session.add(day_total)
     
     # Guardar los cambios en DayTotal
     db.session.commit()
     
+    # Proceso de MonthTotal
+    month_total = MonthTotal.query.filter_by(month=month).first()
+    if not month_total:
+        # Si no existe un registro de MonthTotal, lo creamos
+        month_total = MonthTotal(total=total_sum, month=month)
+        db.session.add(month_total)
+    else:
+        # Si ya existe, lo actualizamos sumando los valores a total
+        month_total.total += total_sum
+
+    # Guardar los cambios en MonthTotal
+    db.session.commit()
+
     # Devolver la respuesta con los datos de WallData
     return jsonify(data.to_json()), 201
  
@@ -123,37 +171,30 @@ def read():
 @app.route(BASE_URL + '/readDayTotal', methods=['GET'])
 def readDayTotal():
     tasks = DayTotal.query.all()
-    #print(tasks)
-   
+
     return jsonify([task.to_json() for task in tasks])
 
+@app.route(BASE_URL + '/readLatestDayTotal', methods=['GET'])
+def readLatestDayTotal():
+    data = DayTotal.query.order_by(DayTotal.id.desc()).first()
+    try:
+        return jsonify(data.to_json())
+    except:
+        return jsonify({'status': "False", 'message': "There is no data"}), 404
+    
 @app.route(BASE_URL + '/readLatest', methods=['GET'])
 def readLatest():
     data = WallData.query.order_by(WallData.id.desc()).first()
     #print(tasks)
    
     return jsonify(data.to_json())
-   
-@app.route(BASE_URL + '/update/<id>', methods=['PUT'])
-def update(id):
-    data = WallData.query.get(id)
-    if not data:
-        abort(400)
-    data.status = not data.status
-    db.session.commit()
-       
-    return jsonify(data.to_json()), 201
- 
- 
-@app.route(BASE_URL + '/delete/<id>', methods=['DELETE'])
-def delete(id):
-    data = WallData.query.get_or_404(id)
 
-    db.session.delete(data)
-    db.session.commit()
-   
-    return jsonify({'status':"True"}), 201
- 
+@app.route(BASE_URL + '/readMonthTotal', methods=['GET'])
+def readMonthTotal():
+    tasks = MonthTotal.query.all()
+
+    return jsonify([task.to_json() for task in tasks])
+
 @app.route(BASE_URL + '/reset', methods=['DELETE'])
 def reset():
     try:
@@ -163,45 +204,19 @@ def reset():
         # Eliminar todos los registros de DayTotal
         DayTotal.query.delete()
         
+        # Eliminar todos los registros de MonthTotal
+        MonthTotal.query.delete()
+
         # Confirmar los cambios
         db.session.commit()
         
         return jsonify({'status': "True", 'message': "All data has been reset"}), 200
     except Exception as e:
         db.session.rollback()  # Hacer rollback en caso de error
-        return jsonify({'status': "False", 'message': str(e)}), 500
-
- 
-def create_day_total():
-    # Esta función se ejecutará cada día a las 00:00
-    today = date.today()
-
-    # Revisa si ya hay un DayTotal creado hoy para evitar duplicados
-    existing_day_total = DayTotal.query.filter_by(created_at=today).first()
-    
-    if not existing_day_total:
-        # Crear un nuevo DayTotal con todos los valores inicializados a 0
-        new_day_total = DayTotal(grupo1=0, grupo2=0, grupo3=0, created_at=today)
-        
-        db.session.add(new_day_total)
-        db.session.commit()
-        print(f"Nuevo DayTotal creado para la fecha {today}")
-    else:
-        print(f"Ya existe un DayTotal para la fecha {today}")
-
-# Inicializar el scheduler
-def start_scheduler():
-    scheduler = BackgroundScheduler()
-    
-    # Programar la tarea para ejecutarse todos los días a las 00:00
-    scheduler.add_job(create_day_total, 'cron', hour=0, minute=0)
-    
-    scheduler.start()
-
+        return jsonify({'status': "False", 'message': str(e)}), 50
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        start_scheduler()  # Iniciar el scheduler
-    print("Tables created and scheduler started...")
+    print("Tables created")
     app.run(debug=False)
